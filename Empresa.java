ad3108c6cfc0ca5d079cafe1f0b1ce7c753a72f2
IsabelaBB <isabela_borlido@hotmail.com>
2017-11-04 16:10:15 -0200

    Add files via upload

---
+++ Empresa.java
@@ -0,0 +1,195 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Empresa extends Pessoa {
+	private String CNPJ;
+
+	public Empresa(String nome, String CNPJ) {
+		super(nome, null);
+		this.setCNPJ(CNPJ);
+	}
+
+	private ArrayList<Prestador> prestadores = new ArrayList<>();
+	private ArrayList<Cliente> clientes = new ArrayList<>();
+	private ArrayList<Servico> listaServicos = new ArrayList<>();
+	private ArrayList<ArrayList<Agenda>> listaAgendas = new ArrayList<>(); //Lista todos os COMPROMISSOS de TODOS os PRESTADORES
+
+	//--inicio Métodos EMPRESA
+	public ArrayList<Prestador> getPrestadores() {
+		return prestadores;
+	}
+
+	public ArrayList<ArrayList<Agenda>> getListaAgendas() {
+		return this.listaAgendas;
+	}	
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+	//--FIM MÉTODOS EMPRESA
+
+	//CRUD
+	//Add
+	//Remove
+	//Atualiza
+	//Lê
+
+	public void cadastra(Pessoa cadastro) {
+		//	caso não retorne uma pessoa, pode cadastrar.
+		if(consultaPessoa(cadastro.getCpf(), cadastro) == null) {
+			if(cadastro instanceof Prestador){
+				prestadores.add(new Prestador(cadastro.getNome(), cadastro.getCpf()));
+			}
+			else{
+				clientes.add(new Cliente(cadastro.getNome(), cadastro.getCpf()));
+			}
+		}
+		else {
+			System.out.println("Pessoa já cadastrada");
+		}
+	}
+
+	public void remove(String cpf){
+
+	}
+
+	// consulta pessoa cliente ou prestador
+	Pessoa consultaPessoa(String cpf, Pessoa quem) {
+		if(quem instanceof Cliente){
+			if(clientes == null)
+				return null;
+			for(Cliente c : clientes) {
+				if(c.getCpf().equals(cpf))
+					return c;
+			}
+		}
+		if(quem instanceof Prestador){
+			if(prestadores == null)
+				return null;
+			for(Prestador p : prestadores) {
+				if(p.getCpf().equals(cpf))
+					return p;
+			}
+		}
+
+		return null;
+	}
+
+	// consulta servico
+	public Servico consultaServico(String nome) {
+		for(Servico i : listaServicos) {
+			if(i.getNome().equalsIgnoreCase(nome))
+				return i;
+		}
+		return null;
+	}
+
+
+	// Adiciona um serviço na lista de serviços que podem ser prestados
+	// necessita de tratamento de exceção
+	public void cadastraServico(String nome, String descricao, int duracao) {
+		if(consultaServico(nome) == null) 
+			listaServicos.add(new Servico(nome, descricao, duracao));
+		else 
+			System.out.println("Serviço já existente!");
+	}
+
+	// Retorna a lista dos prestadores que prestam um determinado serviço e que possuem agenda livre
+	public ArrayList<Prestador> buscarPrestadoresServico(String nomeServico, LocalDate dataInicio){
+		ArrayList<Prestador> listaPrestadores = new ArrayList<Prestador>();
+		Servico servico = consultaServico(nomeServico);
+
+		// criar tratamento de erro para serviço inexistente
+		if(servico == null) 
+			return null;
+
+		// criar tratamento de erro para quando não tem prestadores disponíveis
+		for(Prestador i : prestadores) {
+			if(i.consultaAgenda(dataInicio, servico.getDuracao()) == null) 
+				listaPrestadores.add(i);
+		}
+		return listaPrestadores;
+	}
+
+	public boolean agendarServico(String CpfCliente, String nomeServico, LocalDate dataInicio, Prestador prest) 
+			throws ExcecaoAgendar, ExcecaoPessoa, ExcecaoServico{
+
+		Cliente c = new Cliente(null, CpfCliente);
+		Servico sv = null;
+
+		// testa se o cliente existe
+		c = (Cliente) consultaPessoa(CpfCliente, c);
+		if(c == null)
+			throw new ExcecaoPessoa("Cliente Inexistente !"); 
+		//quem chama o método agendarServico deve tratar com um try-catch para pegar a Exceção.
+		// Logo, como vai ser exibida a mensagem de erro passada não é problema da classe e sim de quem usa o método
+
+
+		// testa se o serviço existe
+		sv = consultaServico(nomeServico);
+		if(sv == null)
+			throw new ExcecaoServico();
+
+		// testa se o prestador existe
+		prest = (Prestador) consultaPessoa(prest.getCpf(), prest);			
+		if(prest == null)
+			throw new ExcecaoPessoa("Prestador Inexistente !");
+
+		//Chegou aqui - registrar serviço no prestador para o cliente passado
+		if(prest.marcarCompromisso(dataInicio, c, sv))
+			return true; 	//sucesso
+		else{
+			return false;	//fail
+		}
+
+	}
+
+
+	private void atualizaAgendaGeral(Prestador p){
+		this.listaAgendas.add(p.getListaCompromissos());
+	}
+
+	public void imprimeTodosCompromisso(){
+		for(int i = 0; i < this.listaAgendas.size(); i ++){
+			System.out.println("\n");
+			for(int j = 0; j < this.listaAgendas.get(i).size(); j ++ ){
+				System.out.println("Cliente: " + listaAgendas.get(i).get(j).getCliente().getNome());
+				System.out.println("Data: " + listaAgendas.get(i).get(j).getData().getDayOfMonth() +
+						"/"+listaAgendas.get(i).get(j).getData().getMonthValue() + "/"+ listaAgendas.get(i).get(j).getData().getYear());
+				System.out.println("Serviço: " + listaAgendas.get(i).get(j).getServiço().getNome());
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	//	MÉTODOS DA EMPRESA
+
+	public String getCNPJ() {
+		return CNPJ;
+	}
+
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+
+	@Override
+	public String getCpf() {
+		return getCNPJ();
+	}
+
+	@Override
+	public void setCpf(String cpf) {
+		setCNPJ(cpf);
+	}
+
+
+}
